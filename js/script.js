260b92a5a3153d42eacd456af82e5d5ec1ab7b06
dariolaseta <dario.laseta@gmail.com>
2023-05-11 14:36:59 +0200

    added password generator

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const userName = prompt("Qual è il tuo nome?");
 const userLastName = prompt("Qual è il tuo cognome?");
 
 const userFavouriteColor = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("generated-password").innerHTML = userName + userLastName + userFavouriteColor + "21";
